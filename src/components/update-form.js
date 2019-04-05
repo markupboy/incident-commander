cb63410227024e8948b2a047987029c90097f8b9
Blake Walters <blake@markupboy.com>
2019-04-05 15:19:06 -0600

    updated form helper text

--- src/components/update-form.js
+++ src/components/update-form.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 
 import { Button, Form } from "react-bootstrap";
+import Container from "./container";
 
 function UpdateForm({ addUpdate, resetUpdates, previousUpdate }) {
   const descriptionRef = React.createRef();
@@ -33,44 +34,47 @@ function UpdateForm({ addUpdate, resetUpdates, previousUpdate }) {
   });
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formDescription">
-        <Form.Label>Incident description</Form.Label>
-        <Form.Control type="text" ref={descriptionRef} />
-        <Form.Text className="text-muted">
-          A smaller than twitter-sized overview of what's occurring, this should
-          rarely change between updates
-        </Form.Text>
-      </Form.Group>
-      <Form.Group controlId="formStatus">
-        <Form.Label>Status</Form.Label>
-        <Form.Control as="select" ref={statusRef}>
-          <option value="" readOnly />
-          <option value="Investigating">Investigating</option>
-          <option value="Identified">Identified</option>
-          <option value="Monitoring">Monitoring</option>
-          <option value="Resolved">Resolved</option>
-        </Form.Control>
-      </Form.Group>
-      <Form.Group controlId="formUpdate">
-        <Form.Label>Update</Form.Label>
-        <Form.Control type="text" ref={messageRef} />
-        <Form.Text className="text-muted">
-          A concise overview of the delta in status since the previous update.
-          This should highlight important changes or why no important changes
-          have been made.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-        &nbsp;
-        <Button variant="secondary" onClick={handleReset}>
-          Reset
-        </Button>
-      </Form.Group>
-    </Form>
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formDescription">
+          <Form.Label>Incident description</Form.Label>
+          <Form.Control type="text" ref={descriptionRef} />
+          <Form.Text className="text-muted">
+            A short, succinct description of the customer impact of the
+            incident. Rarely more than 10 words and should rarely change between
+            updates.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formStatus">
+          <Form.Label>Status</Form.Label>
+          <Form.Control as="select" ref={statusRef}>
+            <option value="" readOnly />
+            <option value="Investigating">Investigating</option>
+            <option value="Identified">Identified</option>
+            <option value="Monitoring">Monitoring</option>
+            <option value="Resolved">Resolved</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="formUpdate">
+          <Form.Label>Update</Form.Label>
+          <Form.Control type="text" ref={messageRef} />
+          <Form.Text className="text-muted">
+            A concise overview of the delta in status since the previous update.
+            This should highlight important changes or why no important changes
+            have been made.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+          &nbsp;
+          <Button variant="secondary" onClick={handleReset}>
+            Reset
+          </Button>
+        </Form.Group>
+      </Form>
+    </Container>
   );
 }
 
